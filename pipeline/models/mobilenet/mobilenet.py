8d6f7ba69d7c40de66e0970752a13526f0cfc34d
SeanSon2005 <astroson2005@gmail.com>
2024-05-03 00:47:05 -0700

    updated to hopefully pass flake8 i pray

---
+++ pipeline/models/mobilenet/mobilenet.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+
+class MobileNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = torch.hub.load(
+            'pytorch/vision:v0.10.0',
+            'mobilenet_v2',
+            pretrained=False)
+
+    def forward(self, x):
+        return self.model(x)
