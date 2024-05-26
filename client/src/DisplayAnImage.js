7f3e839affbee88aadc7fd7f540013d723c31625
David <157341401+davidwang5@users.noreply.github.com>
2024-05-26 09:40:35 -0700

    Update DisplayAnImage.js

--- client/src/DisplayAnImage.js
+++ client/src/DisplayAnImage.js
@@ -4,23 +4,22 @@ import {View, Image, StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
-    paddingLeft: 150,
+    paddingLeft: 0,
   },
   tinyLogo: {
-    width: 500,
+    width: 600,
     height: 500,
-    class:"center"
+    class:"center",
   },
   
 });
-
 
 const DisplayAnImage = () => {
   return (
     <View style={styles.container}>
       
       <Image
-        style={styles.tinyLogo} class="center"
+        style={styles.tinyLogo} class="center" 
         source={{
           uri: 'https://wallpapercave.com/wp/wp9637279.jpg',
         }}
