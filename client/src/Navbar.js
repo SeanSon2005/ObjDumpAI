8da87af4e91d2e49ae36a7b2ed66cf5fe5f2adbe
jmincs <143158613+jmincs@users.noreply.github.com>
2024-05-26 07:59:16 -0700

    Update Navbar.js

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,16 +1,33 @@
+import Login from './Login';
+import React, { useState } from 'react';
+
 const Navbar = () => {
+  const [setToken] = useState();
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = function(){
+    setClicked(true)
+  }
     return (
       <nav className="navbar">
         <h1>AI Object Detection</h1>
         <div className="links">
           <a href="/">Home</a>
           <a href="/">Upload Database</a>
-          <a href="/login" style={{ 
-            color: 'white', 
-            backgroundColor: '#007FFF',
-            borderRadius: '10px' 
-          }}>Login</a>
+          <button 
+            onClick={handleClick}
+            style={{ 
+              color: 'white',
+              backgroundColor: '#007FFF',
+              borderRadius: '10px',
+              padding: '0.5em 1em',
+              textDecoration: 'none',
+              cursor: 'pointer',
+              fontFamily: 'Roboto, sans-serif',
+              marginLeft: '30px' 
+          }}>Login</button>
         </div>
+        {clicked && <Login setToken={setToken} />}
       </nav>
     );
   }
