006d13c34a1e0ead7f8046876c38d54fe8e27a54
David <157341401+davidwang5@users.noreply.github.com>
2024-05-25 22:17:49 -0700

    Update App.js with new UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,97 +1,108 @@
 import Navbar from './Navbar';
 import axios from "axios";
 import React, { Component } from "react";
+import DisplayAnImage from './DisplayAnImage';
  
+var description = "A towering skyscraper rises above the vast city skyline";
+
 class App extends Component {
     state = {
         selectedFile: null,
     };
- 
+
+    
     onFileChange = (event) => {
         this.setState({
             selectedFile: event.target.files[0],
         });
     };
  
-    // On file upload (click the upload button)
     onFileUpload = () => {
-        // Create an object of formData
         const formData = new FormData();
- 
-        // Update the formData object
         formData.append(
             "myFile",
             this.state.selectedFile,
             this.state.selectedFile.name
         );
  
-        // Details of the uploaded file
         console.log(this.state.selectedFile);
  
-        // Request made to the backend api
-        // Send formData object
         axios.post("api/uploadfile", formData);
     };
- 
-    // File content to be displayed after
-    // file upload is complete
+    
     fileData = () => {
-        if (this.state.selectedFile) {
-            return (
-                <div>
-                    <h2>File Details:</h2>
-                    <p>
-                        File Name:{" "}
-                        {this.state.selectedFile.name}
-                    </p>
- 
-                    <p>
-                        File Type:{" "}
-                        {this.state.selectedFile.type}
-                    </p>
- 
-                    <p>
-                        Last Modified:{" "}
-                        {this.state.selectedFile.lastModifiedDate.toDateString()}
-                    </p>
-                    <button onClick={this.onFileUpload}>
-                        Upload!
-                    </button>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <br />
-                    <h4>
-                       
-                    </h4>
-                </div>
-            );
-        }
-    };
- 
+      if (this.state.selectedFile) {
+          return (
+            <center>
+              <div>
+                  <h2>File Details:</h2>
+                  <p>
+                      File Name:{" "}
+                      {this.state.selectedFile.name}
+                  </p>
+
+                  <p>
+                      File Type:{" "}
+                      {this.state.selectedFile.type}
+                  </p>
+
+                  <p>
+                      Last Modified:{" "}
+                      {this.state.selectedFile.lastModifiedDate.toDateString()}
+                  </p>
+                  <button onClick={this.onFileUpload}>
+                    
+                      Upload!
+                  </button>
+              </div>
+              </center>
+          );
+      } else {
+          return (
+              <div>
+                  <br />
+                  <h4>
+                     
+                  </h4>
+              </div>
+          );
+      }
+  };
+
     render() {
         return (
             <div className="App">
                 <Navbar />
+                
             <div className="content">
-            <h2><center>Image Upload</center></h2>
+
             </div>
                 <div>
+                <h2><center>Explore the World with the Power of AI</center></h2>
+                  <div>
+                  {DisplayAnImage()}
+                  </div>
+                </div>
+                
+
+                <h3><center>AI Generated Image Description: </center></h3>
+
+                <h4><center>{description} </center></h4>
+
+                <h5><center>Image Upload</center></h5>
                 <center> <input
                         type="file"
                         onChange={this.onFileChange}
                     />
+
+{this.fileData()}
                     </center>
-               
-                </div>
-                {this.fileData()}
-                
+
+
           </div>
-            
+
         );
     }
 }
- 
+
 export default App;
