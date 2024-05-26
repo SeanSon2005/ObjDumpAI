6cb5aaa1cf2bb53b1bbde7ef0e1424caf0e81886
David <157341401+davidwang5@users.noreply.github.com>
2024-05-26 09:31:29 -0700

    Create Database.js

---
+++ client/src/Database.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react"; 
+import Spacer from './Spacer';
+
+
+function Database() {
+
+    return (
+        <div className="content"> 
+          <h1>Database</h1>
+        </div>
+      );
+    }
+    
+    export default Database; 
