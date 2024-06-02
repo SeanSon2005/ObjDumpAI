276031db39a7239353b247c4901caaebf7670e5e
SeanSon2005 <astroson2005@gmail.com>
2024-06-01 18:33:33 -0700

    fixed pwd mistake in setup.sh

--- environment/setup.sh
+++ environment/setup.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-touch package_versions.txt
-printf "Python: " > package_versions.txt
-python3 --version | cut -c 8- >> package_versions.txt
+touch environment/package_versions.txt
+printf "Python: " > environment/package_versions.txt
+python3 --version | cut -c 8- >> environment/package_versions.txt
 
 sudo apt install python3-django > /dev/null
-printf "Django: " >> package_versions.txt
-django-admin --version >> package_versions.txt
+printf "Django: " >> environment/package_versions.txt
+django-admin --version >> environment/package_versions.txt
 
 sudo apt install python3-pip python3-venv > /dev/null
-python3 -m venv env
-source env/bin/activate
+python3 -m venv environment/env
+source environment/env/bin/activate
 
-pip install -r requirements.txt
-printf "Requirement Versions... " >> package_versions.txt
+pip install -r environment/requirements.txt
+printf "Requirement Versions... " >> environment/package_versions.txt
 
